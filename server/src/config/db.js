6159d91b0355a69528bfcbcca0805b9e7eb10b5f
mightang <mightang@gmail.com>
2025-12-07 03:24:37 +0900

    no message

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,13 +1,21 @@
 // server/config/db.js
 import Database from "better-sqlite3";
 import path from "path";
+import fs from "fs"; 
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // 프로젝트 안에 data/fixer.db 파일을 생성
-const dbPath = path.join(__dirname, "..", "data", "fixer.db");
+const dbDir = path.join(__dirname, "..", "..", "data");
+const dbPath = new Database(dbPath);
+
+// data 디렉토리가 없으면 먼저 만들어 주기
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
+
 const db = new Database(dbPath);
 
 // 테이블 생성 (없으면 생성)
